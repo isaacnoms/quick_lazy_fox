bda617a9f0b5e432097f5742109a7cc109a96a64
Isobel Norris <isobel.norris@gmail.com>
2018-01-19 16:35:01 +1100

    Made generate collections of words shorter than a certain number of characters

--- combined.py
+++ combined.py
@@ -2,6 +2,7 @@
 import random
 
 words_list = []
+cut_off = 50
 phrase = 'the quick brown fox jumps over the lazy dog' #len == 43
 
 with open('words.txt') as words_file:
@@ -15,6 +16,8 @@ def create():
 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	sentence = []
 	while alphabet:
+		if len(' '.join(sentence)) > cut_off:
+			break
 		rand_word = random.choice(words_list)
 		sentence.append(rand_word)
 		for n in range(len(rand_word)):
@@ -28,7 +31,7 @@ def create():
 
 sentence = create()
 
-while len(sentence) > 80:
+while len(sentence) > cut_off:
 	sentence = create()
 
 print(len(sentence))
